e66a80a0b5a54b94a1a4f4494e233fa87a7cf307
bo32 <david.bonnal@gmail.com>
2022-01-30 20:08:20 +0100

    add shuffle option

--- lib/mopidy_http_service.py
+++ lib/mopidy_http_service.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 import requests
 import json
 
@@ -8,6 +9,7 @@ MOPIDY_BROWSE_LIBRARY = 'core.library.browse'
 MOPIDY_TRACKLIST_CLEAR = 'core.tracklist.clear'
 MOPIDY_TRACKLIST_ADD = 'core.tracklist.add'
 MOPIDY_TRACKLIST_SHUFFLE = 'core.tracklist.shuffle'
+MOPIDY_TRACKLIST_GET_TRACKS = 'core.tracklist.get_tl_tracks'
 
 MOPIDY_PLAYBACK_PLAY = 'core.playback.play'
 MOPIDY_PLAYBACK_PAUSE = 'core.playback.pause'
@@ -21,14 +23,20 @@ MUSIC_FOLDER_PLACEHOLDER = '[music_folder]'
 
 LOGGER = logger.get_logger(__name__)
 
+# TODO add some external props/conf for timers and other options
+
+
 class MopidyHttpService:
 
     def __init__(self, host):
+        LOGGER.info("UPDATED")
         self.host = host
         self.currently_playing = None
+        self.is_candidate_for_shuffle = False
+        self.disable_shuffle_proc = None
 
     def play_value(self, value: str):
-        LOGGER.info(value)
+        LOGGER.info("Scanned value to be played: " + value)
         try:
             value_type = int(value[0])
             value_index = int(value[1:])
@@ -36,14 +44,22 @@ class MopidyHttpService:
             LOGGER.warn('Cannot convert integer value')
             return
 
+        LOGGER.info("currently plqying: " + str(self.currently_playing))
+        LOGGER.info("new value: " + value)
         if self.currently_playing == value:
-            if self.is_playing():
-                self.pause()
-                return
-            elif self.is_paused():
-                self.resume()
-                return
-        self.process_value(value_index, value_type)
+            if self.is_candidate_for_shuffle:
+                self.shuffle()
+                self.is_candidate_for_shuffle = False
+                LOGGER.info('here')
+                self.play_tracklist_track_at_index(0)
+            else:
+                if self.is_playing():
+                    self.pause()
+                elif self.is_paused():
+                    self.resume()
+        else:
+            self.currently_playing = value
+            self.process_value(value_index, value_type)
 
     def process_value(self, value_index, value_type):
         if value_type == 1:
@@ -54,8 +70,6 @@ class MopidyHttpService:
             self.play_station_index(value_index)
         else:
             LOGGER.warn('Cannot process {}'.format(str(value_type)))
-        
-        self.currently_playing = value
 
 
     def get_folder_uri_from_index(self, index):
@@ -97,6 +111,25 @@ class MopidyHttpService:
             uri = uri.replace(MUSIC_FOLDER_PLACEHOLDER, music_folder_value)
             LOGGER.info('Updated URI: {}'.format(uri))
         self.play_folder_uri(uri)
+        self.is_candidate_for_shuffle = True
+        self.disable_shuffle_option()
+
+    def disable_shuffle_option(self):
+        LOGGER.info("Starting Shuffle coutdown before being disabled")
+        if not self.disable_shuffle_proc is None and self.disable_shuffle_proc.is_alive():
+            self.disable_shuffle_proc.terminate()
+            LOGGER.info("Current option for disable Shuffle terminated before end of countdown.")
+
+        self.disable_shuffle_proc = mp.Process(target=self.wait_and_disable_shuffle_option)
+        self.disable_shuffle_proc.start()
+
+    def wait_and_disable_shuffle_option(self):
+        import time
+        time.sleep(10)
+
+        LOGGER.info('Shuffle countdown completed')
+        self.is_candidate_for_shuffle = False
+        LOGGER.info('Shuffle disabled')
 
     def play_youtube_playlist_index(self, index: int):
         uri = self.get_youtube_playlist_uri_from_index(index)
@@ -105,6 +138,15 @@ class MopidyHttpService:
 
     def add_uri_to_tracklist(self, uri):
         self.send_modipy_rpc_request(MOPIDY_TRACKLIST_ADD, {'uris': [uri]})
+
+    def get_tracklist_tracks(self):
+        response = self.send_modipy_rpc_request(MOPIDY_TRACKLIST_GET_TRACKS)
+        payload = response.json()
+        return json.loads(json.dumps(payload))['result']
+
+    def play_tracklist_track_at_index(self, index):
+        tracks = self.get_tracklist_tracks()
+        self.play_tracklist_track(tracks[index])
 
     def play_youtube_playlist(self, uri):
         self.stop_and_clear()
@@ -115,6 +157,10 @@ class MopidyHttpService:
     def play(self):
         LOGGER.info('PLAY')
         self.send_modipy_rpc_request(MOPIDY_PLAYBACK_PLAY)
+    
+    def play_tracklist_track(self, track):
+        LOGGER.info('PLAY TRACK ' + str(track))
+        self.send_modipy_rpc_request(MOPIDY_PLAYBACK_PLAY, {'tl_track': track})
 
     def resume(self):
         LOGGER.info('RESUME')
@@ -175,7 +221,6 @@ class MopidyHttpService:
         return self.get_state() == 'paused'
 
     def get_state(self):
-        # response = requests.post(self.host, json={'jsonrpc': '2.0', 'id': '1', 'method': MOPIDY_PLAYBACK_STATE})
         response = self.send_modipy_rpc_request(MOPIDY_PLAYBACK_STATE)
         payload = response.json()
         return json.loads(json.dumps(payload))['result']
